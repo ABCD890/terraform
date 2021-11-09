e6523bb1cbb309a69df76d477e5cf7a413a2e5b3
$KING_TUNAD <olatundealiu552@gmail.com>
2021-11-09 12:25:34 -0600

    change

--- examples/output.tf
+++ examples/output.tf
@@ -1,3 +1,31 @@
+output "abc0" {
+ value = "hello/nworld"
+}
+output "abcc" {
+  value = "hello"
+}
+
+variable "abc" {
+  default = "100"
+}
 output "abc" {
-  value = "hello/nworld"
+  value = "var.abc"
+}
+variable "abc1" {
+  default = 100
+}
+output "abc1" {
+  value = "var.abc1"
+}
+string data type
+variable "samples1" {
+  default = "hello world"
+}
+number data type
+variable "sample2"{
+  dafault = 100
+}
+boolean data type
+variable "sample3" {
+  dafault = true
 }
