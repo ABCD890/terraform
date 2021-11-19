09503d6f890f999bec98d69203ba32e985212897
$KING_TUNAD <olatundealiu552@gmail.com>
2021-11-18 19:40:24 -0600

    make backend file

--- examples/backend.tf
+++ examples/backend.tf
@@ -6,3 +6,18 @@
     region     = "us-east-1"
   }
 }
+
+terraform {
+backend "s3" {
+bucket = "example_tf_states"
+key    = "global/vpc/us_east_1/example_state.tfstate"
+encrypt = true
+region     = "us-east-1"
+}
+}
+
+provider "aws" {
+access_key = "${var.access_key}"
+secret_key = "${var.secret_key}"
+region = "${var.region}"
+}
